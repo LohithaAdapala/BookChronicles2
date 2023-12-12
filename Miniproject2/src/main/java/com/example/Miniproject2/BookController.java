b1f34d5ed511550bd94056cfaa774b70243b7736
LohithaAdapala <lohithaadapala@gmail.com>
2023-12-12 18:56:13 +0530

    CURD operations

---
+++ Miniproject2/src/main/java/com/example/Miniproject2/BookController.java
@@ -0,0 +1,47 @@
+package com.example.Miniproject2;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import com.example.Miniproject2.BookService;
+import com.example.Miniproject2.Book;
+
+import java.util.ArrayList;
+
+@RestController
+public class BookController {
+    @Autowired
+    private BookH2Service bookH2Service;
+    BookService bookService= new BookService();
+    @GetMapping("/books")
+    public ArrayList<Book> getAllBooks(){
+        return bookH2Service.getBooks();
+
+    }
+
+    @GetMapping("/books/{bookId}")
+    public Book getBookById(@PathVariable("bookId") int bookId){
+
+        return bookH2Service.getBookById(bookId);
+    }
+
+    @PostMapping("/books")
+    public Book addBook(@RequestBody Book book){
+        return bookH2Service.addBook(book);
+    }
+
+    @PutMapping("books/{bookId}")
+    public Book updateBook(@PathVariable("bookId") int bookId, @RequestBody Book book){
+        return bookH2Service.updateBook(bookId,book);
+    }
+
+    @DeleteMapping("books/{bookId}")
+    public void deleteBook(@PathVariable("bookId") int bookId){
+        bookH2Service.deleteBook(bookId);
+    }
+
+
+
+
+
+
+}
---
+++ Miniproject2/src/main/resources/schema.sql
@@ -0,0 +1,5 @@
+insert into book(name,imageUrl) values('Lohitha','Lohitha.png');
+insert into book(name,imageUrl) values('Lohitha','Lohitha.png');
+insert into book(name,imageUrl) values('Lohitha','Lohitha.png');
+insert into book(name,imageUrl) values('Lohitha','Lohitha.png');
+insert into book(name,imageUrl) values('Lohitha','Lohitha.png');
